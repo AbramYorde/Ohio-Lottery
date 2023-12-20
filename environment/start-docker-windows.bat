917af5a3ee8ac910b123315edd72e570f3eaddf1
Abram Yorde <33256566+AbramYorde@users.noreply.github.com>
2023-12-20 10:22:14 -0500

    Delete environment directory

--- environment/start-docker-windows.bat
+++
@@ -1,8 +0,0 @@
-docker build . -f Dockerfile -t dslp/default
-cd ..
-docker run --rm -d ^
-	-p 8787:8787 ^
-	-e ROOT=true -e DISABLE_AUTH=true ^
-	--mount type=bind,source=%cd%,target=/home/rstudio/ ^
-	dslp/default 
-PAUSE
